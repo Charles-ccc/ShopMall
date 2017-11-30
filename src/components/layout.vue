99b75025dbcaa3d8c8a9f7ebac3b65a88ee5a0fa
Charles-ccc <liu930505@qq.com>
2017-11-30 19:59:05 +0800

    tests

---
+++ src/components/layout.vue
@@ -0,0 +1,90 @@
+<template>
+  <!-- <div>
+      <div class="app-header">
+          <div class="app-head-inner">
+              <img src="../assets/logo" alt="LOGO">
+              <div class="head-nav">
+                  <ul class="nav-list">
+                      <li>登录</li>
+                      <li class="nav-pile">|</li>
+                      <li>注册</li>
+                      <li class="nav-pile">|</li>
+                      <li>登录</li>
+                  </ul>
+              </div>
+          </div>
+      </div>
+      <div class="app-content">Content</div>
+      <div class="app-footer">
+          <p>© 2017 LiuChang Write</p>
+      </div>
+  </div> -->
+</template>
+
+<style scoped>
+    /* body {
+        background: #f0f2f5;
+        font-family: "Helvetica Neue",Helvetica,Arial,"Hiragino Sans GB","Hiragino Sans GB W3","Microsoft YaHei UI","Microsoft YaHei","WenQuanYi Micro Hei",sans-serif;
+        font-size: 14px;
+        color: #444;
+    }
+    .app-head {
+    background: #363636;
+    color: #b2b2b2;
+    height: 90px;
+    line-height: 90px;
+    width: 100%;
+    }
+    .app-head-inner {
+    width: 1200px;
+    margin: 0 auto;
+    }
+    .head-logo {
+    float: left;
+    }
+    .app-head-inner img {
+    width: 50px;
+    margin-top: 20px;
+    }
+    .head-nav {
+    float: right;
+    }
+    .head-nav ul {
+    overflow: hidden;
+    }
+    .head-nav li {
+    cursor: pointer;
+    float: left;
+    }
+    .nav-pile {
+    padding: 0 10px;
+    }
+    .app-foot {
+    text-align: center;
+    height: 80px;
+    width: 100%;
+    line-height: 80px;
+    background: #e3e4e8;
+    clear: both;
+    margin-top: 30px;
+    }
+    .container {
+    width: 1200px;
+    margin: 0 auto;
+    }
+    .hr {
+    height: 1px;
+    width: 100%;
+    background: #ddd;
+    } */
+</style>
+
+<script>
+    /* export default {
+        data(){
+            return{
+                msg:"Hello World"
+            }
+        }
+    } */
+</script> 
